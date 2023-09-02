f2bc55a2b6d9541a7f1a41b4f77a339b7e45a243
DarkPhoenix1000 <82330796+DarkPhoenix1000@users.noreply.github.com>
2023-09-03 03:53:26 +0900

    Update mismagius.asm

--- data/pokemon/dex_entries/silver/mismagius.asm
+++ data/pokemon/dex_entries/silver/mismagius.asm
@@ -1,1 +1,11 @@
+	db "MAGICAL@" ; species name
+	dw 211, 97 ; height, weight
+
+	db   "Its cries sounds"
+	next "like incantations"
+	next "Those hearing it"
+
+	page "are tormented by"
+	next "headaches and"
+	next "hallucinations.@"
 
