68b219c172591c458d7acc2f62e0704cb9068fee
DarkPhoenix1000 <82330796+DarkPhoenix1000@users.noreply.github.com>
2023-09-02 20:11:25 +0900

    Update pokemon_constants.asm

--- constants/pokemon_constants.asm
+++ constants/pokemon_constants.asm
@@ -266,8 +266,8 @@ DEF JOHTO_POKEMON EQU const_value
 	const LUGIA      ; f9
 	const HO_OH      ; fa
 	const CELEBI     ; fb
+        const MISMAGIUS  ; fc
 DEF NUM_POKEMON EQU const_value - 1
-	const_skip       ; fc
 	const EGG        ; fd
 
 ; Unown forms
