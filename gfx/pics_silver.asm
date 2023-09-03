123156eeb044312dbf96d1b15ad24697204fd4e5
DarkPhoenix1000 <82330796+DarkPhoenix1000@users.noreply.github.com>
2023-09-03 21:25:34 +0900

    Update pics_silver.asm

--- gfx/pics_silver.asm
+++ gfx/pics_silver.asm
@@ -692,7 +692,6 @@ UnownIBackpic:       INCBIN "gfx/pokemon/unown_i/back.2bpp.lz"
 UnownIFrontpic:      INCBIN "gfx/pokemon/unown_i/front.2bpp.lz"
 UnownRBackpic:       INCBIN "gfx/pokemon/unown_r/back.2bpp.lz"
 UnownRFrontpic:      INCBIN "gfx/pokemon/unown_r/front.2bpp.lz"
-MismagiusFrontpic:   INCBIN "gfx/pokemon/mismagius/front_silver.2bpp.1z
-MismagiusBackpic:    INCBIN "gfx/pokemon/mismagius/back.2bpp.1z   
 
+SECTION "Pics 15", ROMX
      
